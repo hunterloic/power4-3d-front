0ac033671c7beb89786926d7a9585c5cd6901dba
dev1 <you@example.com>
2020-07-12 18:54:49 +0900

    add online feature

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const http = require('http');
 const app = require('./app');
+const socketio = require('socket.io');
 
 const port = process.env.POWER4_FRONT_PORT || 3001;
 
 const server = http.createServer(app);
+const io = socketio(server);
 
 server.listen(port);
